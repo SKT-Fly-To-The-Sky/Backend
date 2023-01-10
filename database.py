600f135315a22644ab47c9e5d2ba89456c457e8b
DW-K <pch145@naver.com>
2023-01-10 18:45:14 +0900

    update sql url

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:0000@host.docker.internal:3306/ftts?charset=utf8mb4"
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:0000@172.23.240.72:3306/ftts?charset=utf8mb4"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:0000@210.109.63.74:11004/ftts?charset=utf8mb4"
 # SQLALCHEMY_DATABASE_URL = "mysql+pymysql://connectuser:0000@127.0.0.1:3306/ftts?charset=utf8mb4"
 
 engine = create_engine(
