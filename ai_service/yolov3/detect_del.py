dc0092d9afbe12ff05d2f305a73168b80f82c84e
Jeong-Su-Lee <jeonogsoo0929@gmail.com>
2023-02-13 18:38:23 +0900

    michin

--- ai_service/yolov3/detect_del.py
+++ ai_service/yolov3/detect_del.py
@@ -112,12 +112,11 @@ def detect(path, img0):
     img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
     img = np.ascontiguousarray(img)
 
-    for path, img in dataset:
-        img = torch.from_numpy(img).to(device)
-        img = img.half() if half else img.float()  # uint8 to fp16/32
-        img /= 255.0  # 0 - 255 to 0.0 - 1.0
-        if img.ndimension() == 3:
-            img = img.unsqueeze(0)
+    img = torch.from_numpy(img).to(device)
+    img = img.half() if half else img.float()  # uint8 to fp16/32
+    img /= 255.0  # 0 - 255 to 0.0 - 1.0
+    if img.ndimension() == 3:
+        img = img.unsqueeze(0)
     # Inference
     pred = model(img, augment=opt.augment)[0]
 
