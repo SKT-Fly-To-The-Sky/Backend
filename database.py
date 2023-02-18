cbb8f70a08c8e9e8ec7343b331da6e7f5bff000f
DW-K <pch145@naver.com>
2023-02-18 17:45:46 +0900

    update docker-compose

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine, Column, Integer, String, LargeBinary
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("mysql+mysqlconnector://root:kaykay2412@localhost/ftts")
+# engine = create_engine("mysql+mysqlconnector://root:kaykay2412@localhost/ftts")
+engine = create_engine("mysql+mysqlconnector://root:0000@jeongsuri.iptime.org:10012/ftts?charset=utf8mb4")
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
