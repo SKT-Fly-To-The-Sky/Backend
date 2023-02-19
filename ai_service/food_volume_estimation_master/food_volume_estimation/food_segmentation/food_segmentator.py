6714e838ab4cc707bc9a68746bc78ed90c5df972
DW-K <pch145@naver.com>
2023-02-19 15:35:09 +0900

    update food_volume module path

--- ai_service/food_volume_estimation_master/food_volume_estimation/food_segmentation/food_segmentator.py
+++ ai_service/food_volume_estimation_master/food_volume_estimation/food_segmentation/food_segmentator.py
@@ -1,11 +1,11 @@
 import sys
 import os
 # from mrcnn.config import Config
-from mrcnn.config import Config
+from ai_service.food_volume_estimation_master.food_volume_estimation.food_segmentation.mrcnn.config import Config
 # from food_segmentation.mrcnn import (
 #     model as modellib,
 #     utils)
-import mrcnn.model as modellib, mrcnn.utils
+import ai_service.food_volume_estimation_master.food_volume_estimation.food_segmentation.mrcnn.model as modellib, mrcnn.utils
 
 # Using the single-cluster model
 clusters = ['food']
