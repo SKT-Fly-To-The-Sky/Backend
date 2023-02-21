742246f4d5dd58b1a06b9f91e097ccf8dcae1e66
DW-K <pch145@naver.com>
2023-02-21 18:04:28 +0900

    y5

--- download_model.sh
+++ download_model.sh
@@ -1,3 +1,6 @@
 curl -L -o ./ai_service/yolov3/weights/best_403food_e200b150v2.pt https://www.dropbox.com/s/msz9yfrmsrs0zst/best_403food_e200b150v2.pt?dl=0
+
 curl -L -o ./ai_service/food_volume_estimation_master/food_volume_estimation/monovideo_fine_tune_food_videos.h5 https://www.dropbox.com/s/zqo3qfzoy7b9spp/monovideo_fine_tune_food_videos.h5?dl=0
 curl -L -o ./ai_service/food_volume_estimation_master/food_volume_estimation/mask_rcnn_food_segmentation.h5 https://www.dropbox.com/s/uewabex707xh2n0/mask_rcnn_food_segmentation.h5?dl=0
+
+curl -L -o ./ai_service/yolov5/best_hoon.pt https://www.dropbox.com/s/veswtugc57so0cb/best_hoon.pt?dl=0
