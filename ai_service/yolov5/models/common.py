f1cdf3242eeacd722a091dee871254bcdac47d35
DW-K <pch145@naver.com>
2023-02-21 18:41:22 +0900

    y5

--- ai_service/yolov5/models/common.py
+++ ai_service/yolov5/models/common.py
@@ -25,13 +25,13 @@ from IPython.display import display
 from PIL import Image
 from torch.cuda import amp
 
-from utils import TryExcept
-from utils.dataloaders import exif_transpose, letterbox
-from utils.general import (LOGGER, ROOT, Profile, check_requirements, check_suffix, check_version, colorstr,
+from ..utils import TryExcept
+from ..utils.dataloaders import exif_transpose, letterbox
+from ..utils.general import (LOGGER, ROOT, Profile, check_requirements, check_suffix, check_version, colorstr,
                            increment_path, is_notebook, make_divisible, non_max_suppression, scale_boxes, xywh2xyxy,
                            xyxy2xywh, yaml_load)
-from utils.plots import Annotator, colors, save_one_box
-from utils.torch_utils import copy_attr, smart_inference_mode
+from ..utils.plots import Annotator, colors, save_one_box
+from ..utils.torch_utils import copy_attr, smart_inference_mode
 
 
 def autopad(k, p=None, d=1):  # kernel, padding, dilation
