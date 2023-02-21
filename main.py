d4dac1820689dac6b9c8590f1954eb3ff6f63b4a
DW-K <pch145@naver.com>
2023-02-21 11:04:57 +0900

    master

--- main.py
+++ main.py
@@ -437,7 +437,8 @@ async def read_recommanded_supplement(userid: str, db: Session = Depends(get_db)
     ).first().image
     encoded_image = base64.b64encode(img).decode('utf-8')
 
-    return {'sup_num': 2, "supplements": [{"image": encoded_image, "name": "영양제1", "link": "https//www.naver.com"},{"image": encoded_image, "name": "영양제2", "link": "https//www.google.com"}]}
+    # return {'sup_num': 2, "supplements": [{"image": encoded_image, "name": "영양제1", "link": "https//www.naver.com"},{"image": encoded_image, "name": "영양제2", "link": "https//www.google.com"}]}
+    return [{"image": encoded_image, "name": "영양제1", "link": "https//www.naver.com"}, {"image": encoded_image, "name": "영양제2", "link": "https//www.google.com"}]
 
 @app.post("/supplements/classification")
 async def read_supplements_classification(file: UploadFile = File(...), db: Session = Depends(get_db)):
