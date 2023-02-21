db5c734850133195d85ce9ce32419868e57df05c
Jeong-Su-Lee <jeonogsoo0929@gmail.com>
2023-02-21 18:58:03 +0900

    genius

--- ai_service/yolov5/utils/general.py
+++ ai_service/yolov5/utils/general.py
@@ -37,8 +37,8 @@ import torch
 import torchvision
 import yaml
 
-from utils.downloads import curl_download, gsutil_getsize
-from utils.metrics import box_iou, fitness
+from .downloads import curl_download, gsutil_getsize
+from .metrics import box_iou, fitness
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[1]  # YOLOv5 root directory
