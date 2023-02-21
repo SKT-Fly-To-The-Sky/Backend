06a9a1b57ca3d460c9669c2b0bcf36fe2f26f430
Jeong-Su-Lee <jeonogsoo0929@gmail.com>
2023-02-21 18:39:07 +0900

    good

--- ai_service/yolov5/detect.py
+++ ai_service/yolov5/detect.py
@@ -39,11 +39,11 @@ import numpy as np
 import PIL
 import io
 
-FILE = Path(__file__).resolve()
-ROOT = FILE.parents[0]  # YOLOv5 root directory
-if str(ROOT) not in sys.path:
-    sys.path.append(str(ROOT))  # add ROOT to PATH
-ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+# FILE = Path(__file__).resolve()
+# ROOT = FILE.parents[0]  # YOLOv5 root directory
+# if str(ROOT) not in sys.path:
+#     sys.path.append(str(ROOT))  # add ROOT to PATH
+# ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
 from .models.common import DetectMultiBackend
 from .utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadScreenshots, LoadStreams
