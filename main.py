f43fd31a4901f581b73be84304d40320792a0d23
DW-K <pch145@naver.com>
2023-02-21 10:46:51 +0900

    master

--- main.py
+++ main.py
@@ -225,8 +225,8 @@ async def get_volume_test(file: UploadFile = File(...), db: Session = Depends(ge
     try:
         image = await file.read()
         pil_image = Image.open(BytesIO(image))
-        output = BytesIO()
-        pil_image.save(output, format='JPEG')
+        # output = BytesIO()
+        # pil_image.save(output, format='JPEG')
         # content = output.getvalue()
     except Exception as e:
         logger.exception(f"create_food_item fail:\n\t{e}\nWrong image")
@@ -234,8 +234,8 @@ async def get_volume_test(file: UploadFile = File(...), db: Session = Depends(ge
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Wrong image")
 
     try:
-        type(pil_image)
-        content = np.array(Image.open(io.BytesIO(pil_image)))
+        print(type(pil_image))
+        content = np.array(pil_image)
         result = qual(content)
         result['running_time'] = time.time() - st
         return JSONResponse(content=result)
