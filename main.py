032576d3555c5f97fbccfa95aa7c4c8e25db4e2f
DW-K <pch145@naver.com>
2023-02-21 10:48:51 +0900

    master

--- main.py
+++ main.py
@@ -224,7 +224,7 @@ async def get_volume_test(file: UploadFile = File(...), db: Session = Depends(ge
     st = time.time()
     try:
         image = await file.read()
-        pil_image = Image.open(BytesIO(image))
+        # pil_image = Image.open(BytesIO(image))
         # output = BytesIO()
         # pil_image.save(output, format='JPEG')
         # content = output.getvalue()
@@ -234,8 +234,8 @@ async def get_volume_test(file: UploadFile = File(...), db: Session = Depends(ge
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Wrong image")
 
     try:
-        print(type(pil_image))
-        content = np.array(pil_image)
+        print(type(image))
+        content = np.array(Image.open(io.BytesIO(image)))
         result = qual(content)
         result['running_time'] = time.time() - st
         return JSONResponse(content=result)
