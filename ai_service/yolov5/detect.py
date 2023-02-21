f8466e93821975910de801f151b3afb404b9b865
Jeong-Su-Lee <jeonogsoo0929@gmail.com>
2023-02-21 19:25:55 +0900

    genius

--- ai_service/yolov5/detect.py
+++ ai_service/yolov5/detect.py
@@ -171,7 +171,7 @@ def detect(path, img0):
         img = img.unsqueeze(0)
 
     with dt[0]:
-        im = torch.from_numpy(im).to(model.device)
+        im = torch.from_numpy(img).to(model.device)
         im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
         im /= 255  # 0 - 255 to 0.0 - 1.0
         if len(im.shape) == 3:
