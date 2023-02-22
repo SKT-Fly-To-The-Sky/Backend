6b8e82abe57a22d59f49db3623ad0b9dffd036ac
Jeong-Su-Lee <jeonogsoo0929@gmail.com>
2023-02-22 17:00:31 +0900

    good

--- ai_service/yolov5/detect_55.py
+++ ai_service/yolov5/detect_55.py
@@ -4,15 +4,16 @@ from pathlib import Path
 
 import torch
 import torch.backends.cudnn as cudnn
+import numpy as np
 from numpy import random
 from PIL import Image
 import cv2
 from ai_service.yolov5.models.experimental import attempt_load
 from ai_service.yolov5.utils.general import check_img_size, non_max_suppression
 from ai_service.yolov5.utils.torch_utils import select_device
-from ai_service.yolov5.models.yolo import yolo_head
 from ai_service.yolov5.utils.general import non_max_suppression
 from ai_service.yolov5.utils.torch_utils import select_device
+from ai_service.yolov5.utils.augmentations import letterbox
 
 def scale_coords(img_shape, coords, im_shape):
     # Rescale x, y, w, h from [0, 1] to image dimensions
@@ -39,14 +40,13 @@ def plot_one_box(x, img, color=None, label=None, line_thickness=None):
         cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
         cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
 
-
 def detect_ljs(image_path):
-    weights = 'yolov5s.pt'  # path to the weights file
+    weights = './ai_service/yolov5/yolov5s.pt'  # path to the weights file
     device = select_device('')  # set device to default
-    model = torch.load(weights, map_location=device)['model'].float()  # load model
-    model.to(device).eval()  # set device and model to evaluation mode
+    model = attempt_load(weights, map_location=device)  # load model
     imgsz = check_img_size(640, s=model.stride.max())  # check image size
     half = device.type != 'cpu'  # set half precision
+    classes = model.names
 
     if half:
         model.half()  # convert model to half-precision floating point for faster inference
@@ -61,29 +61,40 @@ def detect_ljs(image_path):
 
         # Inference
         with torch.no_grad():
-            pred = model(img)
-            det = yolo_head(pred, anchors, 80, imgsz)  # YOLO head operation
-            det = non_max_suppression(det, conf_thres=0.25, iou_thres=0.45)
+            pred = model(img, augment=False)[0]  # run inference
 
-        # Process detections
-        results = []
-        for i, det in enumerate(det):  # detections per image
-            if det is not None and len(det):
-                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], img.shape[2:]).round()  # rescale boxes
-                for *xyxy, conf, cls in reversed(det):
-                    label = f'{classes[int(cls)]} {conf:.2f}'
-                    results.append({'bbox': xyxy, 'class': classes[int(cls)], 'conf': conf})
+        # Apply NMS
+        pred = non_max_suppression(pred, conf_thres=0.25, iou_thres=0.45, classes=None, agnostic=False)
 
-        return results
+        return pred[0] if len(pred) else None
 
     # Load image
-    img0 = cv2.imread(image_path)  # BGR format
+    img0 = cv2.imread(image_path)  # load image using OpenCV
+    img = img0[:, :, ::-1]  # BGR to RGB
+    img = letterbox(img, new_shape=imgsz)[0]  # resize image to model input size while maintaining aspect ratio
+    img = img.transpose(2, 0, 1)  # convert to tensor format
+    img = np.ascontiguousarray(img)
 
-    # Inference
-    img = img0[:, :, ::-1]  # RGB format
-    results = run_inference(img)
+    # Run inference
+    pred = run_inference(img)
 
-    return results
+    # Process detection results
+    if pred is not None:
+        # Rescale coordinates to original image size
+        pred[:, :4] = scale_coords(img.shape[2:], pred[:, :4], img0.shape).round()
+
+        # Extract box coordinates, confidence scores, and class IDs
+        boxes = pred[:, :4].astype(int)
+        scores = pred[:, 4]
+        class_ids = pred[:, 5].astype(int)
+
+        # Print results
+        for i, (box, score, class_id) in enumerate(zip(boxes, scores, class_ids)):
+            print(f'Box {i}: {box}, Class: {classes[class_id]}, Confidence: {score}')
+
+    else:
+        print('No objects detected.')
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
