cfdea17548df219c460c25f8223c21c7dfd0ce58
Jeong-Su-Lee <jeonogsoo0929@gmail.com>
2023-02-22 13:42:40 +0900

    genius

--- ai_service/yolov5/models/experimental.py
+++ ai_service/yolov5/models/experimental.py
@@ -76,6 +76,7 @@ def attempt_load(weights, device=None, inplace=True, fuse=True):
 
     model = Ensemble()
     for w in weights if isinstance(weights, list) else [weights]:
+        print(w)
         ckpt = torch.load(attempt_download(w), map_location='cpu')  # load
         print("good")
         ckpt = (ckpt.get('ema') or ckpt['model']).to(device).float()  # FP32 model
