0978caf90ef311b0cde5c69f03e259b621aba167
Jeong-Su-Lee <jeonogsoo0929@gmail.com>
2023-02-22 21:36:58 +0900

    good

--- ai_service/yolov5/models/experimental.py
+++ ai_service/yolov5/models/experimental.py
@@ -78,10 +78,6 @@ def attempt_load(weights, device=None, inplace=True, fuse=True):
     model = Ensemble()
     for w in weights if isinstance(weights, list) else [weights]:
         ckpt = torch.load(attempt_download(w), map_location=device)  # load
-        
-        print("=============================================================")
-        # print(ckpt)
-        # print("=============================================================")
         ckpt = (ckpt.get('ema') or ckpt['model']).to(device).float()  # FP32 model
         
         # Model compatibility updates
