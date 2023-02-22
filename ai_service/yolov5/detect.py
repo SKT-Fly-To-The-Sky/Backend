4d6d510c066bb7c70958fb480bff712c86555427
Jeong-Su-Lee <jeonogsoo0929@gmail.com>
2023-02-22 15:52:56 +0900

    genius

--- ai_service/yolov5/detect.py
+++ ai_service/yolov5/detect.py
@@ -66,7 +66,7 @@ def scale_coords(img_shape, coords, org_shape):
     return coords
 
 
-def detect(image_path, weights_path, conf_thres=0.25, iou_thres=0.45):
+def good(image_path, weights_path, conf_thres=0.25, iou_thres=0.45):
     # Load model
     device = select_device('')
     model = attempt_load(weights_path)
