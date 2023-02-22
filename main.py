b6da35ee6a8243528766483ed130cf3311ee4588
Jeong-Su-Lee <jeonogsoo0929@gmail.com>
2023-02-22 14:43:37 +0900

    genius

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ from ai_service.food_volume_estimation_master.food_volume_estimation.volume_esti
 from ai_service.supplement_classification.supplement_classifier import sup_classification
 from ai_service.yolov3.detect_del import classification
 from ai_service.yolov5.detect import classification_yolov5
-from ai_service.yolov5.yolov5_detect import detect
+from ai_service.yolov5.yolov5 import detect
 from database import engine, Base, get_db, init_db
 from PIL import Image
 from io import BytesIO
