136b040721c62ec1a7320a70e72712d1030cc5dc
Jeong-Su-Lee <jeonogsoo0929@gmail.com>
2023-02-22 21:00:10 +0900

    good

--- ai_service/yolov5/models/common.py
+++ ai_service/yolov5/models/common.py
@@ -344,12 +344,13 @@ class DetectMultiBackend(nn.Module):
         if pt:  # PyTorch
             try:
                 model = attempt_load(weights if isinstance(weights, list) else w, device=device, inplace=True, fuse=fuse)
-                stride = max(int(model.stride.max()), 32)  # model stride
-                names = model.module.names if hasattr(model, 'module') else model.names  # get class names
-                model.half() if fp16 else model.float()
-                self.model = model  # explicitly assign for to(), cpu(), cuda(), half()
             except Exception as e:
                 print(e)
+            stride = max(int(model.stride.max()), 32)  # model stride
+            names = model.module.names if hasattr(model, 'module') else model.names  # get class names
+            model.half() if fp16 else model.float()
+            self.model = model  # explicitly assign for to(), cpu(), cuda(), half()
+            
         elif jit:  # TorchScript
             LOGGER.info(f'Loading {w} for TorchScript inference...')
             extra_files = {'config.txt': ''}  # model metadata
