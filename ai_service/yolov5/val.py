9498242027d5c61b14b3a6b5a7c6b12870a31e7a
Jeong-Su-Lee <jeonogsoo0929@gmail.com>
2023-02-22 11:45:12 +0900

    genius

--- ai_service/yolov5/val.py
+++ ai_service/yolov5/val.py
@@ -36,15 +36,15 @@ if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
-from models.common import DetectMultiBackend
-from utils.callbacks import Callbacks
-from utils.dataloaders import create_dataloader
-from utils.general import (LOGGER, TQDM_BAR_FORMAT, Profile, check_dataset, check_img_size, check_requirements,
+from ai_service.yolov5.models.common import DetectMultiBackend
+from ai_service.yolov5.utils.callbacks import Callbacks
+from ai_service.yolov5.utils.dataloaders import create_dataloader
+from ai_service.yolov5.utils.general import (LOGGER, TQDM_BAR_FORMAT, Profile, check_dataset, check_img_size, check_requirements,
                            check_yaml, coco80_to_coco91_class, colorstr, increment_path, non_max_suppression,
                            print_args, scale_boxes, xywh2xyxy, xyxy2xywh)
-from utils.metrics import ConfusionMatrix, ap_per_class, box_iou
-from utils.plots import output_to_target, plot_images, plot_val_study
-from utils.torch_utils import select_device, smart_inference_mode
+from ai_service.yolov5.utils.metrics import ConfusionMatrix, ap_per_class, box_iou
+from ai_service.yolov5.utils.plots import output_to_target, plot_images, plot_val_study
+from ai_service.yolov5.utils.torch_utils import select_device, smart_inference_mode
 
 
 def save_one_txt(predn, save_conf, shape, file):
