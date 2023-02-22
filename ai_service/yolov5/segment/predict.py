99834eb70ed71448c6d63711e4eac4d2f59b73c3
Jeong-Su-Lee <jeonogsoo0929@gmail.com>
2023-02-22 11:52:35 +0900

    genius

--- ai_service/yolov5/segment/predict.py
+++ ai_service/yolov5/segment/predict.py
@@ -42,14 +42,14 @@ if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
-from models.common import DetectMultiBackend
-from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadScreenshots, LoadStreams
-from utils.general import (LOGGER, Profile, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
+from ai_service.yolov5.models.common import DetectMultiBackend
+from ai_service.yolov5.utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadScreenshots, LoadStreams
+from ai_service.yolov5.utils.general import (LOGGER, Profile, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
                            increment_path, non_max_suppression, print_args, scale_boxes, scale_segments,
                            strip_optimizer)
-from utils.plots import Annotator, colors, save_one_box
-from utils.segment.general import masks2segments, process_mask, process_mask_native
-from utils.torch_utils import select_device, smart_inference_mode
+from ai_service.yolov5.utils.plots import Annotator, colors, save_one_box
+from ai_service.yolov5.utils.segment.general import masks2segments, process_mask, process_mask_native
+from ai_service.yolov5.utils.torch_utils import select_device, smart_inference_mode
 
 
 @smart_inference_mode()
