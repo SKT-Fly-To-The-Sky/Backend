85af0bd83bf781720e1566bf832d4702c1ab58ec
Jeong-Su-Lee <jeonogsoo0929@gmail.com>
2023-02-22 12:36:05 +0900

    genius

--- ai_service/yolov5/models/common.py
+++ ai_service/yolov5/models/common.py
@@ -341,12 +341,12 @@ class DetectMultiBackend(nn.Module):
         if not (pt or triton):
             w = attempt_download(w)  # download if not local
 
-        if pt:  # PyTorch
-            model = attempt_load(weights if isinstance(weights, list) else w, device=device, inplace=True, fuse=fuse)
-            stride = max(int(model.stride.max()), 32)  # model stride
-            names = model.module.names if hasattr(model, 'module') else model.names  # get class names
-            model.half() if fp16 else model.float()
-            self.model = model  # explicitly assign for to(), cpu(), cuda(), half()
+        # if pt:  # PyTorch
+        #     model = attempt_load(weights if isinstance(weights, list) else w, device=device, inplace=True, fuse=fuse)
+        #     stride = max(int(model.stride.max()), 32)  # model stride
+        #     names = model.module.names if hasattr(model, 'module') else model.names  # get class names
+        #     model.half() if fp16 else model.float()
+        #     self.model = model  # explicitly assign for to(), cpu(), cuda(), half()
         # elif jit:  # TorchScript
         #     LOGGER.info(f'Loading {w} for TorchScript inference...')
         #     extra_files = {'config.txt': ''}  # model metadata
