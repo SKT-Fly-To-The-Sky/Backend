aa15f9920e9875ae6be43494fdb342d8a7b3eddf
Jeong-Su-Lee <jeonogsoo0929@gmail.com>
2023-02-22 14:42:27 +0900

    genius

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ from ai_service.food_volume_estimation_master.food_volume_estimation.volume_esti
 from ai_service.supplement_classification.supplement_classifier import sup_classification
 from ai_service.yolov3.detect_del import classification
 from ai_service.yolov5.detect import classification_yolov5
+from ai_service.yolov5.yolov5_detect import detect
 from database import engine, Base, get_db, init_db
 from PIL import Image
 from io import BytesIO
@@ -219,7 +220,7 @@ async def get_classification_test(file: UploadFile = File(...), db: Session = De
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Wrong image")
 
     try:
-        result = classification_yolov5(content)
+        result = detect('/ai_service/yolov5/data/images/images.jpg','/ai_service/yolov5/best_hoon.pt')
         result['object_num'] = len(result['object'])
         result['running_time'] = time.time() - st
         return JSONResponse(content=result)
