9a73b56791a3aa8cf791b8694590050d4b235e8b
Jeong-Su-Lee <jeonogsoo0929@gmail.com>
2023-02-22 16:07:26 +0900

    genius

--- ai_service/yolov5/models/experimental.py
+++ ai_service/yolov5/models/experimental.py
@@ -9,6 +9,7 @@ import torch
 import torch.nn as nn
 
 from ai_service.yolov5.utils.downloads import attempt_download
+from ai_service.yolov5.models.yolo import Detect, Model
 
 
 class Sum(nn.Module):
@@ -72,7 +73,7 @@ class Ensemble(nn.ModuleList):
 
 def attempt_load(weights, device=None, inplace=True, fuse=True):
     # Loads an ensemble of models weights=[a,b,c] or a single model weights=[a] or weights=a
-    from ai_service.yolov5.models.yolo import Detect, Model
+
 
     model = Ensemble()
     for w in weights if isinstance(weights, list) else [weights]:
