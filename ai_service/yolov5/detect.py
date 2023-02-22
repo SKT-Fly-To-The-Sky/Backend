45171676a948975e3ede3157c41c4d724980f375
Jeong-Su-Lee <jeonogsoo0929@gmail.com>
2023-02-22 15:24:56 +0900

    genius

--- ai_service/yolov5/detect.py
+++ ai_service/yolov5/detect.py
@@ -52,37 +52,6 @@ from ai_service.yolov5.utils.general import (LOGGER, Profile, check_file, check_
 from ai_service.yolov5.utils.plots import Annotator, colors, save_one_box
 from ai_service.yolov5.utils.torch_utils import select_device, smart_inference_mode
 from ai_service.yolov5.models.experimental import attempt_load
-def letterbox(img, new_shape=(416, 416), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True):
-    # Resize image to a 32-pixel-multiple rectangle https://github.com/ultralytics/yolov3/issues/232
-    shape = img.shape[:2]  # current shape [height, width]
-    if isinstance(new_shape, int):
-        new_shape = (new_shape, new_shape)
-
-    # Scale ratio (new / old)
-    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
-    if not scaleup:  # only scale down, do not scale up (for better test mAP)
-        r = min(r, 1.0)
-
-    # Compute padding
-    ratio = r, r  # width, height ratios
-    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
-    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
-    if auto:  # minimum rectangle
-        dw, dh = np.mod(dw, 32), np.mod(dh, 32)  # wh padding
-    elif scaleFill:  # stretch
-        dw, dh = 0.0, 0.0
-        new_unpad = new_shape
-        ratio = new_shape[0] / shape[1], new_shape[1] / shape[0]  # width, height ratios
-
-    dw /= 2  # divide padding into 2 sides
-    dh /= 2
-
-    if shape[::-1] != new_unpad:  # resize
-        img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
-    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
-    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
-    img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
-    return img, ratio, (dw, dh)
 
 def good(image_path, weights_path, conf_thres=0.25, iou_thres=0.45, device=''):
     # Initialize
@@ -119,7 +88,7 @@ def good(image_path, weights_path, conf_thres=0.25, iou_thres=0.45, device=''):
         if det is not None and len(det):
             # Rescale boxes from img_size to original image size
             det[:, :4] = scale_segments(img.shape[2:], det[:, :4], img.shape[2:]).round()
-            
+    print(det)
     # Return the detections
     return pred
 
@@ -200,127 +169,109 @@ def detect_v5(img0):
     # Run inference
     model.warmup(imgsz=(1 if pt or model.triton else bs, 3, *imgsz))  # warmup
     seen, windows, dt = 0, [], (Profile(), Profile(), Profile())
-    # for path, im, im0s, vid_cap, s in dataset:
+    for path, im, im0s, vid_cap, s in dataset:
+        with dt[0]:
+            im = torch.from_numpy(img).to(model.device)
+            im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
+            im /= 255  # 0 - 255 to 0.0 - 1.0
+            if len(im.shape) == 3:
+                im = im[None]  # expand for batch dim
 
-    img0 = np.array(PIL.Image.open(io.BytesIO(img0)))
-    for i in range(len(img0)):
-        for j in range(len(img0[0])):
-            img0[i][j] = img0[i][j][::-1]
+        # Inference
+        with dt[1]:
+            visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
+            pred = model(im, augment=augment, visualize=visualize)
 
-    img = letterbox(img0, new_shape=imgsz)[0]
+        # NMS
+        with dt[2]:
+            pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
 
-    # Convert
-    img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
-    img = np.ascontiguousarray(img)
+        # Second-stage classifier (optional)
+        # pred = utils.general.apply_classifier(pred, classifier_model, im, im0s)
 
-    img = torch.from_numpy(img).to(device)
-    img = img.half() if half else img.float()  # uint8 to fp16/32
-    img /= 255.0  # 0 - 255 to 0.0 - 1.0
-    if img.ndimension() == 3:
-        img = img.unsqueeze(0)
+        # Process predictions
+        for i, det in enumerate(pred):  # per image
+            seen += 1
+            # if webcam:  # batch_size >= 1
+            #     p, im0, frame = path[i], im0s[i].copy(), dataset.count
+            #     s += f'{i}: '
+            # else:
+            p, im0, frame = path, img0.copy(), getattr(dataset, 'frame', 0)
 
-    with dt[0]:
-        im = torch.from_numpy(img).to(model.device)
-        im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
-        im /= 255  # 0 - 255 to 0.0 - 1.0
-        if len(im.shape) == 3:
-            im = im[None]  # expand for batch dim
+            p = Path(p)  # to Path
+            save_path = str(save_dir / p.name)  # im.jpg
+            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # im.txt
+            s += '%gx%g ' % im.shape[2:]  # print string
+            # gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            # imc = im0.copy() if save_crop else im0  # for save_crop
+            annotator = Annotator(im0, line_width=line_thickness, example=str(names))
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], im0.shape).round()
 
-    # Inference
-    with dt[1]:
-        visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
-        pred = model(im, augment=augment, visualize=visualize)
+                # Print results
+                for c in det[:, 5].unique():
+                    n = (det[:, 5] == c).sum()  # detections per class
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
 
-    # NMS
-    with dt[2]:
-        pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+                data_send = {}
+                data_send["object"] = []
+                score=[]
+                total = []
+                object_names = []
+                count = 0
 
-    # Second-stage classifier (optional)
-    # pred = utils.general.apply_classifier(pred, classifier_model, im, im0s)
+                # Write results
+                for *xyxy, conf, cls in reversed(det):
+                    # if save_txt:  # Write to file
+                    #     xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                    #     line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
+                    #     with open(f'{txt_path}.txt', 'a') as f:
+                    #         f.write(('%g ' * len(line)).rstrip() % line + '\n')
 
-    # Process predictions
-    for i, det in enumerate(pred):  # per image
-        seen += 1
-        # if webcam:  # batch_size >= 1
-        #     p, im0, frame = path[i], im0s[i].copy(), dataset.count
-        #     s += f'{i}: '
-        # else:
-        p, im0, frame = path, img0.copy(), getattr(dataset, 'frame', 0)
-
-        p = Path(p)  # to Path
-        save_path = str(save_dir / p.name)  # im.jpg
-        txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # im.txt
-        s += '%gx%g ' % im.shape[2:]  # print string
-        # gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
-        # imc = im0.copy() if save_crop else im0  # for save_crop
-        annotator = Annotator(im0, line_width=line_thickness, example=str(names))
-        if len(det):
-            # Rescale boxes from img_size to im0 size
-            det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], im0.shape).round()
-
-            # Print results
-            for c in det[:, 5].unique():
-                n = (det[:, 5] == c).sum()  # detections per class
-                s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
-
-            data_send = {}
-            data_send["object"] = []
-            score=[]
-            total = []
-            object_names = []
-            count = 0
-
-            # Write results
-            for *xyxy, conf, cls in reversed(det):
-                # if save_txt:  # Write to file
-                #     xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                #     line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
-                #     with open(f'{txt_path}.txt', 'a') as f:
-                #         f.write(('%g ' * len(line)).rstrip() % line + '\n')
-
-                if save_img or save_crop or view_img:  # Add bbox to image
-                    c = int(cls)  # integer class
-                    label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
-                    annotator.box_label(xyxy, label, color=colors(c, True))
-                # if save_crop:
-                #     save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
-                semi=[]
-                score = round(float(conf),2)
-                for nums in range(4):  ## total??좌표 ?�??
-                    str_x = str(xyxy[nums]).split('(')
-                    str_x = str_x[1].split('.')
-                    semi.append(str_x[0])
-                total.append(semi)
-                object_names.append(names[c])
-                count = count + 1
-            for i in range(count):  ##리스트 두 개 xml파일에 저장
-                data_send["object"].append({
-                    "name" : object_names[i],
-                    "bndbox":{
-                    "xmin": str(total[i][0]),
-                    "ymin": str(total[i][1]),
-                    "xmax": str(total[i][2]),
-                    "ymax": str(total[i][3])
-                    },
-                    "score":score
-                })
-            # with open(save_path[:save_path.rfind('.')] + '.json', 'w') as outfile:
-            #     json.dump(data_send, outfile)
-    if data_send:
-        print(data_send)
-        return "0000"
-    else:
-        data_send["object"].append({
-                    "name" : '0000000',
-                    "bndbox":{
-                    "xmin": '0',
-                    "ymin": '0',
-                    "xmax": '0',
-                    "ymax": '0'
-                    },
-                    "score":'0'
-                })
-        return data_send
+                    if save_img or save_crop or view_img:  # Add bbox to image
+                        c = int(cls)  # integer class
+                        label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
+                        annotator.box_label(xyxy, label, color=colors(c, True))
+                    # if save_crop:
+                    #     save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
+                    semi=[]
+                    score = round(float(conf),2)
+                    for nums in range(4):  ## total??좌표 ?�??
+                        str_x = str(xyxy[nums]).split('(')
+                        str_x = str_x[1].split('.')
+                        semi.append(str_x[0])
+                    total.append(semi)
+                    object_names.append(names[c])
+                    count = count + 1
+                for i in range(count):  ##리스트 두 개 xml파일에 저장
+                    data_send["object"].append({
+                        "name" : object_names[i],
+                        "bndbox":{
+                        "xmin": str(total[i][0]),
+                        "ymin": str(total[i][1]),
+                        "xmax": str(total[i][2]),
+                        "ymax": str(total[i][3])
+                        },
+                        "score":score
+                    })
+                # with open(save_path[:save_path.rfind('.')] + '.json', 'w') as outfile:
+                #     json.dump(data_send, outfile)
+        if data_send:
+            print(data_send)
+            return "0000"
+        else:
+            data_send["object"].append({
+                        "name" : '0000000',
+                        "bndbox":{
+                        "xmin": '0',
+                        "ymin": '0',
+                        "xmax": '0',
+                        "ymax": '0'
+                        },
+                        "score":'0'
+                    })
+            return data_send
 
         # Stream results
     #     im0 = annotator.result()
