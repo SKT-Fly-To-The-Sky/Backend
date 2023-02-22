bb883f53bbfe35e826404cbef2d806f1fa34ca6c
Jeong-Su-Lee <jeonogsoo0929@gmail.com>
2023-02-22 12:16:20 +0900

    genius

--- ai_service/yolov5/export.py
+++ ai_service/yolov5/export.py
@@ -246,118 +246,118 @@ def export_coreml(model, im, file, int8, half, prefix=colorstr('CoreML:')):
     return f, ct_model
 
 
-# @try_export
-# def export_engine(model, im, file, half, dynamic, simplify, workspace=4, verbose=False, prefix=colorstr('TensorRT:')):
-#     # YOLOv5 TensorRT export https://developer.nvidia.com/tensorrt
-#     assert im.device.type != 'cpu', 'export running on CPU but must be on GPU, i.e. `python export.py --device 0`'
-#     try:
-#         import tensorrt as trt
-#     except Exception:
-#         if platform.system() == 'Linux':
-#             check_requirements('nvidia-tensorrt', cmds='-U --index-url https://pypi.ngc.nvidia.com')
-#         import tensorrt as trt
+@try_export
+def export_engine(model, im, file, half, dynamic, simplify, workspace=4, verbose=False, prefix=colorstr('TensorRT:')):
+    # YOLOv5 TensorRT export https://developer.nvidia.com/tensorrt
+    assert im.device.type != 'cpu', 'export running on CPU but must be on GPU, i.e. `python export.py --device 0`'
+    try:
+        import tensorrt as trt
+    except Exception:
+        if platform.system() == 'Linux':
+            check_requirements('nvidia-tensorrt', cmds='-U --index-url https://pypi.ngc.nvidia.com')
+        import tensorrt as trt
 
-#     if trt.__version__[0] == '7':  # TensorRT 7 handling https://github.com/ultralytics/yolov5/issues/6012
-#         grid = model.model[-1].anchor_grid
-#         model.model[-1].anchor_grid = [a[..., :1, :1, :] for a in grid]
-#         export_onnx(model, im, file, 12, dynamic, simplify)  # opset 12
-#         model.model[-1].anchor_grid = grid
-#     else:  # TensorRT >= 8
-#         check_version(trt.__version__, '8.0.0', hard=True)  # require tensorrt>=8.0.0
-#         export_onnx(model, im, file, 12, dynamic, simplify)  # opset 12
-#     onnx = file.with_suffix('.onnx')
+    if trt.__version__[0] == '7':  # TensorRT 7 handling https://github.com/ultralytics/yolov5/issues/6012
+        grid = model.model[-1].anchor_grid
+        model.model[-1].anchor_grid = [a[..., :1, :1, :] for a in grid]
+        export_onnx(model, im, file, 12, dynamic, simplify)  # opset 12
+        model.model[-1].anchor_grid = grid
+    else:  # TensorRT >= 8
+        check_version(trt.__version__, '8.0.0', hard=True)  # require tensorrt>=8.0.0
+        export_onnx(model, im, file, 12, dynamic, simplify)  # opset 12
+    onnx = file.with_suffix('.onnx')
 
-#     LOGGER.info(f'\n{prefix} starting export with TensorRT {trt.__version__}...')
-#     assert onnx.exists(), f'failed to export ONNX file: {onnx}'
-#     f = file.with_suffix('.engine')  # TensorRT engine file
-#     logger = trt.Logger(trt.Logger.INFO)
-#     if verbose:
-#         logger.min_severity = trt.Logger.Severity.VERBOSE
+    LOGGER.info(f'\n{prefix} starting export with TensorRT {trt.__version__}...')
+    assert onnx.exists(), f'failed to export ONNX file: {onnx}'
+    f = file.with_suffix('.engine')  # TensorRT engine file
+    logger = trt.Logger(trt.Logger.INFO)
+    if verbose:
+        logger.min_severity = trt.Logger.Severity.VERBOSE
 
-#     builder = trt.Builder(logger)
-#     config = builder.create_builder_config()
-#     config.max_workspace_size = workspace * 1 << 30
-#     # config.set_memory_pool_limit(trt.MemoryPoolType.WORKSPACE, workspace << 30)  # fix TRT 8.4 deprecation notice
+    builder = trt.Builder(logger)
+    config = builder.create_builder_config()
+    config.max_workspace_size = workspace * 1 << 30
+    # config.set_memory_pool_limit(trt.MemoryPoolType.WORKSPACE, workspace << 30)  # fix TRT 8.4 deprecation notice
 
-#     flag = (1 << int(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH))
-#     network = builder.create_network(flag)
-#     parser = trt.OnnxParser(network, logger)
-#     if not parser.parse_from_file(str(onnx)):
-#         raise RuntimeError(f'failed to load ONNX file: {onnx}')
+    flag = (1 << int(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH))
+    network = builder.create_network(flag)
+    parser = trt.OnnxParser(network, logger)
+    if not parser.parse_from_file(str(onnx)):
+        raise RuntimeError(f'failed to load ONNX file: {onnx}')
 
-#     inputs = [network.get_input(i) for i in range(network.num_inputs)]
-#     outputs = [network.get_output(i) for i in range(network.num_outputs)]
-#     for inp in inputs:
-#         LOGGER.info(f'{prefix} input "{inp.name}" with shape{inp.shape} {inp.dtype}')
-#     for out in outputs:
-#         LOGGER.info(f'{prefix} output "{out.name}" with shape{out.shape} {out.dtype}')
+    inputs = [network.get_input(i) for i in range(network.num_inputs)]
+    outputs = [network.get_output(i) for i in range(network.num_outputs)]
+    for inp in inputs:
+        LOGGER.info(f'{prefix} input "{inp.name}" with shape{inp.shape} {inp.dtype}')
+    for out in outputs:
+        LOGGER.info(f'{prefix} output "{out.name}" with shape{out.shape} {out.dtype}')
 
-#     if dynamic:
-#         if im.shape[0] <= 1:
-#             LOGGER.warning(f'{prefix} WARNING ⚠️ --dynamic model requires maximum --batch-size argument')
-#         profile = builder.create_optimization_profile()
-#         for inp in inputs:
-#             profile.set_shape(inp.name, (1, *im.shape[1:]), (max(1, im.shape[0] // 2), *im.shape[1:]), im.shape)
-#         config.add_optimization_profile(profile)
+    if dynamic:
+        if im.shape[0] <= 1:
+            LOGGER.warning(f'{prefix} WARNING ⚠️ --dynamic model requires maximum --batch-size argument')
+        profile = builder.create_optimization_profile()
+        for inp in inputs:
+            profile.set_shape(inp.name, (1, *im.shape[1:]), (max(1, im.shape[0] // 2), *im.shape[1:]), im.shape)
+        config.add_optimization_profile(profile)
 
-#     LOGGER.info(f'{prefix} building FP{16 if builder.platform_has_fast_fp16 and half else 32} engine as {f}')
-#     if builder.platform_has_fast_fp16 and half:
-#         config.set_flag(trt.BuilderFlag.FP16)
-#     with builder.build_engine(network, config) as engine, open(f, 'wb') as t:
-#         t.write(engine.serialize())
-#     return f, None
+    LOGGER.info(f'{prefix} building FP{16 if builder.platform_has_fast_fp16 and half else 32} engine as {f}')
+    if builder.platform_has_fast_fp16 and half:
+        config.set_flag(trt.BuilderFlag.FP16)
+    with builder.build_engine(network, config) as engine, open(f, 'wb') as t:
+        t.write(engine.serialize())
+    return f, None
 
 
-# @try_export
-# def export_saved_model(model,
-#                        im,
-#                        file,
-#                        dynamic,
-#                        tf_nms=False,
-#                        agnostic_nms=False,
-#                        topk_per_class=100,
-#                        topk_all=100,
-#                        iou_thres=0.45,
-#                        conf_thres=0.25,
-#                        keras=False,
-#                        prefix=colorstr('TensorFlow SavedModel:')):
-#     # YOLOv5 TensorFlow SavedModel export
-#     try:
-#         import tensorflow as tf
-#     except Exception:
-#         check_requirements(f"tensorflow{'' if torch.cuda.is_available() else '-macos' if MACOS else '-cpu'}")
-#         import tensorflow as tf
-#     from tensorflow.python.framework.convert_to_constants import convert_variables_to_constants_v2
+@try_export
+def export_saved_model(model,
+                       im,
+                       file,
+                       dynamic,
+                       tf_nms=False,
+                       agnostic_nms=False,
+                       topk_per_class=100,
+                       topk_all=100,
+                       iou_thres=0.45,
+                       conf_thres=0.25,
+                       keras=False,
+                       prefix=colorstr('TensorFlow SavedModel:')):
+    # YOLOv5 TensorFlow SavedModel export
+    try:
+        import tensorflow as tf
+    except Exception:
+        check_requirements(f"tensorflow{'' if torch.cuda.is_available() else '-macos' if MACOS else '-cpu'}")
+        import tensorflow as tf
+    from tensorflow.python.framework.convert_to_constants import convert_variables_to_constants_v2
 
-#     from models.tf import TFModel
+    from models.tf import TFModel
 
-#     LOGGER.info(f'\n{prefix} starting export with tensorflow {tf.__version__}...')
-#     f = str(file).replace('.pt', '_saved_model')
-#     batch_size, ch, *imgsz = list(im.shape)  # BCHW
+    LOGGER.info(f'\n{prefix} starting export with tensorflow {tf.__version__}...')
+    f = str(file).replace('.pt', '_saved_model')
+    batch_size, ch, *imgsz = list(im.shape)  # BCHW
 
-#     tf_model = TFModel(cfg=model.yaml, model=model, nc=model.nc, imgsz=imgsz)
-#     im = tf.zeros((batch_size, *imgsz, ch))  # BHWC order for TensorFlow
-#     _ = tf_model.predict(im, tf_nms, agnostic_nms, topk_per_class, topk_all, iou_thres, conf_thres)
-#     inputs = tf.keras.Input(shape=(*imgsz, ch), batch_size=None if dynamic else batch_size)
-#     outputs = tf_model.predict(inputs, tf_nms, agnostic_nms, topk_per_class, topk_all, iou_thres, conf_thres)
-#     keras_model = tf.keras.Model(inputs=inputs, outputs=outputs)
-#     keras_model.trainable = False
-#     keras_model.summary()
-#     if keras:
-#         keras_model.save(f, save_format='tf')
-#     else:
-#         spec = tf.TensorSpec(keras_model.inputs[0].shape, keras_model.inputs[0].dtype)
-#         m = tf.function(lambda x: keras_model(x))  # full model
-#         m = m.get_concrete_function(spec)
-#         frozen_func = convert_variables_to_constants_v2(m)
-#         tfm = tf.Module()
-#         tfm.__call__ = tf.function(lambda x: frozen_func(x)[:4] if tf_nms else frozen_func(x), [spec])
-#         tfm.__call__(im)
-#         tf.saved_model.save(tfm,
-#                             f,
-#                             options=tf.saved_model.SaveOptions(experimental_custom_gradients=False) if check_version(
-#                                 tf.__version__, '2.6') else tf.saved_model.SaveOptions())
-#     return f, keras_model
+    tf_model = TFModel(cfg=model.yaml, model=model, nc=model.nc, imgsz=imgsz)
+    im = tf.zeros((batch_size, *imgsz, ch))  # BHWC order for TensorFlow
+    _ = tf_model.predict(im, tf_nms, agnostic_nms, topk_per_class, topk_all, iou_thres, conf_thres)
+    inputs = tf.keras.Input(shape=(*imgsz, ch), batch_size=None if dynamic else batch_size)
+    outputs = tf_model.predict(inputs, tf_nms, agnostic_nms, topk_per_class, topk_all, iou_thres, conf_thres)
+    keras_model = tf.keras.Model(inputs=inputs, outputs=outputs)
+    keras_model.trainable = False
+    keras_model.summary()
+    if keras:
+        keras_model.save(f, save_format='tf')
+    else:
+        spec = tf.TensorSpec(keras_model.inputs[0].shape, keras_model.inputs[0].dtype)
+        m = tf.function(lambda x: keras_model(x))  # full model
+        m = m.get_concrete_function(spec)
+        frozen_func = convert_variables_to_constants_v2(m)
+        tfm = tf.Module()
+        tfm.__call__ = tf.function(lambda x: frozen_func(x)[:4] if tf_nms else frozen_func(x), [spec])
+        tfm.__call__(im)
+        tf.saved_model.save(tfm,
+                            f,
+                            options=tf.saved_model.SaveOptions(experimental_custom_gradients=False) if check_version(
+                                tf.__version__, '2.6') else tf.saved_model.SaveOptions())
+    return f, keras_model
 
 
 # @try_export
