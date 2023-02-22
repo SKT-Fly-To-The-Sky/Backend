76ad05863f754fb5754daa4ecc5a2aa6840aab33
Jeong-Su-Lee <jeonogsoo0929@gmail.com>
2023-02-22 12:41:05 +0900

    genius

--- ai_service/yolov5/models/experimental.py
+++ ai_service/yolov5/models/experimental.py
@@ -84,10 +84,11 @@ def attempt_load(weights, device=None, inplace=True, fuse=True):
         if not hasattr(ckpt, 'stride'):
             ckpt.stride = torch.tensor([32.])
         if hasattr(ckpt, 'names') and isinstance(ckpt.names, (list, tuple)):
+            print("good")
             ckpt.names = dict(enumerate(ckpt.names))  # convert to dict
 
         model.append(ckpt.fuse().eval() if fuse and hasattr(ckpt, 'fuse') else ckpt.eval())  # model in eval mode
-    return "123"
+    return "good"
 
     # Module compatibility updates
     for m in model.modules():
