a38407fb88b03f1d7360bdd57e44e07a0c2fd25d
Jeong-Su-Lee <jeonogsoo0929@gmail.com>
2023-02-22 11:18:16 +0900

    genius

--- ai_service/yolov5/models/common.py
+++ ai_service/yolov5/models/common.py
@@ -604,9 +604,9 @@ class DetectMultiBackend(nn.Module):
         # from ai_service.yolov5.export import export_formats
         from ai_service.yolov5.utils.downloads import is_url
         # sf = list(export_formats().Suffix)  # export suffixes
-        # if not is_url(p, check=False):
-        #     check_suffix(p, sf)  # checks
-        # url = urlparse(p)  # if url may be Triton inference server
+        if not is_url(p, check=False):
+             check_suffix(p, sf)  # checks
+        url = urlparse(p)  # if url may be Triton inference server
         # types = [s in Path(p).name for s in sf]
         # types[8] &= not types[9]  # tflite &= not edgetpu
         # triton = not any(types) and all([any(s in url.scheme for s in ['http', 'grpc']), url.netloc])
