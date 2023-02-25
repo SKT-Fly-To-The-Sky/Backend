8a84f9ce45f08e3bfa2844385cd4ef0ff02b61b9
psmin0325 <psmin9325@gmail.com>
2023-02-25 15:21:17 +0900

    update dockerfile and detect.py

--- ai_service/yolov5/detect.py
+++ ai_service/yolov5/detect.py
@@ -57,7 +57,7 @@ from utils.torch_utils import select_device, smart_inference_mode
 @smart_inference_mode()
 def run(
         img0=None,
-        weights=ROOT / 'yolov5s.pt',  # model path or triton URL
+        weights=ROOT / 'yolov5x.pt',  # model path or triton URL
         source=ROOT / 'data/images',  # file/dir/URL/glob/screen/0(webcam)
         data=ROOT / 'data/coco128.yaml',  # dataset.yaml path
         imgsz=(640, 640),  # inference size (height, width)
