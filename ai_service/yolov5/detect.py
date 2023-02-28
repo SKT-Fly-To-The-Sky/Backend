8a910edd83cd64f24e8aeafc73f1de8e9905f04c
DW-K <pch145@naver.com>
2023-02-28 14:15:42 +0900

    add init page

--- ai_service/yolov5/detect.py
+++ ai_service/yolov5/detect.py
@@ -298,7 +298,7 @@ def main(opt):
     return run(**opt)
 
 
-def classification_yolov5(img0, weight_path = 'best_dinner.pt'):
+def classification_yolov5(img0, weight_path ='best_dinner.pt'):
     opt = parse_opt(img0=img0, weight_path=weight_path)
     img, result = main(opt)
     return result
